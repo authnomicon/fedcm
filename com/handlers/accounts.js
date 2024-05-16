9d387321f58440a7dc7aca022a29ebf6a48ab82d
Jared Hanson <jaredhanson@gmail.com>
2024-05-16 15:09:31 -0700

    Clean up.

--- com/handlers/accounts.js
+++ com/handlers/accounts.js
@@ -1,7 +1,5 @@
 exports = module.exports = function(authenticator) {
   
-  // TODO: auth this request and respond with accounts
-  
   function notLoggedIn(req, res, next) {
     if (!req.user) {
       return res.json({ accounts: [] });
@@ -10,19 +8,14 @@ exports = module.exports = function(authenticator) {
   }
   
   function accounts(req, res, next) {
-    console.log('ACCOUNTS!');
-    console.log(req.user);
     var users = req.user;
     if (!Array.isArray(users)) {
       users = [ users ];
     }
-    console.log(users);
     
     // NOTE: responding with empty array when logged in (as indicated by the Set-Login: logged-in header)
     // causes Chrome to display an option to launch the login_url
     //res.json({ accounts: [] })
-    
-    //return;
     
     var accounts = []
       , i = 0;
@@ -31,9 +24,6 @@ exports = module.exports = function(authenticator) {
       
       var user = users[i++];
       if (!user) {
-        console.log('RESPONDING WITH');
-        console.log(accounts);
-        
         // done, respond
         return res.json({
           accounts: accounts
@@ -48,23 +38,10 @@ exports = module.exports = function(authenticator) {
       accounts.push(account);
       iter();
     })();
-    
-    return;
-    
-    //var users
-    
-    
-    res.json({ accounts: [{
-      id: '1234',
-      given_name: 'John',
-      name: 'John Doe',
-      email: 'johndoe@example.com'
-    }]})
   }
   
   
   return [
-    //authenticator.authenticate([ 'session', 'anonymous' ], { multi: true }),
     authenticator.authenticate([ 'session', 'anonymous' ], { multi: true }),
     notLoggedIn,
     accounts
