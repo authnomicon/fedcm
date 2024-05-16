eb9de1e9222defca4607cf9b15ca1b16cad8b451
Jared Hanson <jaredhanson@gmail.com>
2024-05-16 13:01:05 -0700

    Add tests.

--- test/package.test.js
+++ test/package.test.js
@@ -4,25 +4,16 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 
 
-describe('@authnomicon/openidconnect', function() {
+describe('@authnomicon/fedcm', function() {
   
   describe('package.json', function() {
     var json = require('../package.json');
     
     it('should have assembly metadata', function() {
-      expect(json.assembly.namespace).to.equal('org.authnomicon/openidconnect');
+      expect(json.assembly.namespace).to.equal('org.authnomicon/fedcm');
       expect(json.assembly.components).to.deep.equal([
-        'oauth2/authorize/http/request/openid',
-        'oauth2/authorize/http/response/types/codetoken',
-        'oauth2/authorize/http/response/types/codeidtokentoken',
-        'oauth2/authorize/http/response/types/codeidtoken',
-        'oauth2/authorize/http/response/types/idtoken',
-        'oauth2/authorize/http/response/types/idtokentoken',
-        'oauth2/authorize/http/response/sessionstate',
-        'oauth2/token/http/response/idtoken',
-        'userinfo/http/service',
-        'logout/http/service',
-        'session/http/service'
+        'service',
+        'well-known/service'
       ]);
     });
   });
