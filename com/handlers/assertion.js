8d61376d6fe969f6e3eb8761379644735c7e0b56
Jared Hanson <jaredhanson@gmail.com>
2024-05-16 15:23:39 -0700

    Set CORS headers in assertion response.

--- com/handlers/assertion.js
+++ com/handlers/assertion.js
@@ -5,6 +5,14 @@ exports = module.exports = function() {
   // TODO: auth this request and respond with accounts
   
   function config(req, res, next) {
+    console.log('RESPOND WITH TOKEN!');
+    console.log(req.headers)
+    console.log(req.body);
+    
+    // These headers need to be set, otherwise the browser fails
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    
     res.json({
       token: 'eyDSE'
     })
@@ -12,6 +20,7 @@ exports = module.exports = function() {
   
   
   return [
+    require('body-parser').urlencoded({ extended: false }),
     config
   ];
 };
