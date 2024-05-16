dd4a03bee3028bc68059540136d43dfaf429108c
Jared Hanson <jaredhanson@gmail.com>
2024-05-16 13:32:33 -0700

    Add tests.

---
+++ test/service.test.js
@@ -0,0 +1,26 @@
+/* global describe, it, expect */
+
+var expect = require('chai').expect;
+var factory = require('../com/service');
+
+
+describe('service', function() {
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.deep.equal('http://i.bixbyjs.org/http/Service');
+    expect(factory['@path']).to.equal('/web-identity');
+  });
+  
+  it('should create service', function() {
+    function configHandler() {};
+    function accountsHandler() {};
+    function clientMetadataHandler() {};
+    function assertionHandler() {};
+  
+    var service = factory(configHandler, accountsHandler, clientMetadataHandler, assertionHandler);
+    
+    expect(service).to.be.a('function');
+    expect(service.length).to.equal(3);
+  });
+  
+});
