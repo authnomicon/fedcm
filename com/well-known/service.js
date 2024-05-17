75b589d9cc0984af62325a14f8b9139a728991c6
Jared Hanson <jaredhanson@gmail.com>
2024-05-17 14:53:24 -0700

    Add JSDoc.

--- com/well-known/service.js
+++ com/well-known/service.js
@@ -1,5 +1,13 @@
+// Module dependencies.
 var express = require('express');
 
+/**
+ * Create FedCM well-known service.
+ *
+ * @param {express.RequestHandler} manifestHandler - Handler which responds with
+ *          the well-known file.
+ * @returns {express.Router}
+ */
 exports = module.exports = function(manifestHandler) {
   var router = express.Router();
   router.get('/', manifestHandler);
@@ -7,6 +15,7 @@ exports = module.exports = function(manifestHandler) {
   return router;
 };
 
+// Module annotations.
 exports['@implements'] = 'http://i.bixbyjs.org/http/Service';
 exports['@path'] = '/.well-known/web-identity';
 exports['@require'] = [
