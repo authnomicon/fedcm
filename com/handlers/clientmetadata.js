694a0984ed1428207acff1826e3e6300d8129a76
Jared Hanson <jaredhanson@gmail.com>
2024-05-20 13:47:56 -0700

    Add tests.

--- com/handlers/clientmetadata.js
+++ com/handlers/clientmetadata.js
@@ -2,6 +2,8 @@ exports = module.exports = function(clients) {
   
   function metadata(req, res, next) {
     clients.read(req.query.client_id, function(err, client) {
+      if (err) { return next(err); }
+      
       var meta = {};
       if (client.privacyPolicyURL) { meta.privacy_policy_url = client.privacyPolicyURL; }
       if (client.termsOfServiceURL) { meta.terms_of_service_url = client.termsOfServiceURL; }
--- test/handlers/clientmetadata.test.js
+++ test/handlers/clientmetadata.test.js
@@ -43,6 +43,28 @@ describe('handlers/clientmetadata', function() {
         .listen();
     }); // should respond with privacy policy and terms of service
     
+    it('should respond without privacy policy and terms of service', function(done) {
+      var clients = new Object();
+      clients.read = sinon.stub().yieldsAsync(null, {
+        id: '1234'
+      });
+      var handler = factory(clients);
+    
+      chai.express.use(handler)
+        .request(function(req) {
+          req.query = {};
+          req.query.client_id = '1234';
+        })
+        .finish(function() {
+          expect(clients.read).to.have.been.calledOnceWith('1234');
+          
+          expect(this).to.have.status(200);
+          expect(this).to.have.body({});
+          done();
+        })
+        .listen();
+    }); // should respond without privacy policy and terms of service
+    
   }); // handler
   
 });
